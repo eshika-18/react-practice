1d72406d8888a57174eb485e6a428f4e8595e16f
Eshika <ishika.kasliwal18@gmail.com>
2022-01-28 22:15:23 +0530

    Variable file is created

--- variable.js
+++ variable.js
@@ -1,4 +1,26 @@
-let x = 5;
-let y = 6;
-let z = x + y;
-console.log(z);
+//var x = 20;
+//var y = 30;
+//var z = x + y;
+
+//let x = "5" + 2 + 3;
+
+//let x = 3 + 2 + "5"
+
+/*const x = 3;
+{
+const x = 6;
+}*/
+
+/*var x =3;
+{
+var x=10;
+}*/
+
+let x=3;
+{
+let x=9;
+}
+
+
+
+console.log(x);
